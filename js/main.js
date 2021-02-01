228bcf5068fb2b47c216df9d5f4df0a59026c7a6
EPeng <erpengic@gmail.com>
2021-01-31 21:14:14 -0600

    added form to submit book details, did some css work

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-document.querySelector('#add_book').addEventListener('click', createBook)
-document.querySelector
+document.querySelector('#open_Form').addEventListener('click', openForm)
+document.querySelector('#close_Form').addEventListener('click', closeForm)
+document.querySelector('.submit').addEventListener('click', addBooktoLibrary)
 let myLibrary =[]
 
 class Book{
@@ -14,9 +15,9 @@ class Book{
 //Brings up window to input new book details
 //need to add an enter key with event listener to store inputs into vars
 function createBook(){
-
+    let bookForm = document.createElement('div')
+    bookForm.classList.add('book-form-container')
 }
-
 
 function displayLibrary(){
     myLibrary.forEach(book => {
@@ -26,21 +27,30 @@ function displayLibrary(){
     })
 }
 
+function createBookObj(){
+    return new Book (document.getElementById('book_Title').value, document.getElementbyId('book_Author').value, document.getElementbyId('book_Pages').value, document.getElementbyId('check_Read').value )
+}
 //After filling out new book details, a new block with  book info is displayed to the DOM under class '.book-container'
 function addBooktoLibrary(book){
     myLibrary.push(book)
-    let newBook = document.createElement('div')
-    newBook.classList.add('book-container')
-    document.querySelector('.library-container').appendChild(newBook)
+    let newBookElem = document.createElement('div')
+    newBookElem.classList.add('book-container')
+    document.querySelector('.library-container').appendChild(newBookElem)
     for(const prop in book){
         let propText = document.createTextNode(book[prop])
-        console.log(propText)
         let propElement = document.createElement('p')
         propElement.appendChild(propText)
-        newBook.appendChild(propElement)
+        newBookElem.appendChild(propElement)
     }  
 }
 
+function openForm(){
+    document.getElementById('book_Form').style.display = "block"
+}
+
+function closeForm(){
+    document.getElementById('book_Form').style.display = "none"   
+}
 
 
 
@@ -50,5 +60,8 @@ LordoftheRings = new Book('Fellowship of the Rings', 'J.R.R Tolkien', 1000, true
 oldMan = new Book('The Old Man and the Sea', 'Ernest Hemingway', 300, false)
 
 addBooktoLibrary(oldMan)
+addBooktoLibrary(LordoftheRings)
+addBooktoLibrary(LordoftheRings)
+addBooktoLibrary(LordoftheRings)
 addBooktoLibrary(LordoftheRings)
 
